89b8f672fecdf70bacba4c012febbdb7882af3d1
Ashen Chathuranga <ktauchathuranga@gmail.com>
2024-12-27 08:36:16 +0530

    v0.0.1

---
+++ background.js
@@ -0,0 +1,22 @@
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.contextMenus.create({
+      id: "generateQR",
+      title: "Generate QR Code",
+      contexts: ["selection"]
+    });
+  });
+  
+  chrome.contextMenus.onClicked.addListener((info) => {
+    if (info.menuItemId === "generateQR" && info.selectionText) {
+      // Get the URL for the popup.html page using chrome.runtime.getURL
+      const popupUrl = chrome.runtime.getURL(`popup.html?text=${encodeURIComponent(info.selectionText)}`);
+  
+      chrome.windows.create({
+        url: popupUrl,
+        type: "popup",
+        width: 300,
+        height: 300
+      });
+    }
+  });
+  
